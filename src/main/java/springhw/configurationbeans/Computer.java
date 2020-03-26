97a711146a803fc6701f29d1db2208f8f83902c5
bitlowsky <bitlowsky@gmail.com>
2020-03-26 10:20:29 +0300

    MIcrofixes

--- src/main/java/springhw/configurationbeans/Computer.java
+++ src/main/java/springhw/configurationbeans/Computer.java
@@ -1,18 +1,16 @@
 package springhw.configurationbeans;
 
-public class Computer implements Printerable{
-	private Printerable printer;
-	
-	public Computer(Printerable printer) {
-		super();
-		this.printer = printer;
-	}
+public class Computer implements Printerable {
+    private Printerable printer;
 
-	@Override
-	public void print() {
-		// TODO Auto-generated method stub
-		printer.print();
-	}
-	
-	
+    public Computer(Printerable printer) {
+        super();
+        this.printer = printer;
+    }
+
+    @Override
+    public void print() {
+        printer.print();
+    }
+
 }
